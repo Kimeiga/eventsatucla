60450070744d3446f1676aebe0c74f091d6a1bdc
Hakan Alpay <hak7alp@gmail.com>
2020-09-24 20:17:23 -0700

    firebase config

--- src/db.js
+++ src/db.js
@@ -2,15 +2,6 @@
 import firebase from "firebase/app";
 import "firebase/database";
 
-export const db = firebase
-  .initializeApp({
-    apiKey: "AIzaSyAagwIzP9NTEJOcNqcKab1vMS1YvDWuIqI",
-    authDomain: "eventsatucla.firebaseapp.com",
-    databaseURL: "https://eventsatucla.firebaseio.com",
-    projectId: "eventsatucla",
-    storageBucket: "eventsatucla.appspot.com",
-    messagingSenderId: "272746716705",
-    appId: "1:272746716705:web:af37bb0ed1a4862f5f8d60",
-    measurementId: "G-06Y4FLNCCW",
-  })
-  .database();
+import { firebaseConfig } from "./firebaseConfig";
+
+export const db = firebase.initializeApp(firebaseConfig).database();
