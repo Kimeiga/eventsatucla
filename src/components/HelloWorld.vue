a0ac4ad00f5dc4665ed5da33689e5a5ca3578a08
Hakan Alpay <hak7alp@gmail.com>
2020-09-24 23:42:15 -0700

    click on day brings to day view now

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <div class="hello">
     <!-- {{ events }} -->
     <!-- {{ calendarEvents }} -->
-    <FullCalendar id="calendar" :options="calendarOptions" />
+    <FullCalendar ref="fullCalendar" id="calendar" :options="calendarOptions" />
 
     <!-- <vue-json-pretty :data="events"> </vue-json-pretty> -->
   </div>
@@ -42,22 +42,59 @@ export default {
       return {
         eventLimit: true, // for all non-TimeGrid views
         dayMaxEventRows: true, // for all non-TimeGrid views
+
+        nowIndicator: true,
+        plugins: [dayGridPlugin, interactionPlugin, timeGridPlugin],
+        initialView: "dayGridMonth",
+        events: this.calendarEvents,
+        headerToolbar:
+          window.innerWidth > 500
+            ? {
+                left: "dayGridMonth,timeGridWeek,timeGridDay",
+                center: "title",
+                right: "prevYear,prev,today,next,nextYear",
+              }
+            : {
+                left: "dayGridMonth,timeGridWeek,timeGridDay",
+                center: "title",
+                right: "prev,today,next",
+              },
+
+        titleFormat:
+          window.innerWidth > 500
+            ? { year: "numeric", month: "long", day: "numeric" }
+            : { year: "numeric", month: "short", day: "numeric" },
         views: {
           timeGrid: {
             eventLimit: 6,
             dayMaxEventRows: 6, // adjust to 6 only for timeGridWeek/timeGridDay
           },
+          dayGridMonth: {
+            // name of view
+            titleFormat:
+              window.innerWidth > 500
+                ? { year: "numeric", month: "long" }
+                : { year: "numeric", month: "short" },
+            // other view-specific options here
+          },
         },
-        nowIndicator: true,
-        plugins: [dayGridPlugin, interactionPlugin, timeGridPlugin],
-        initialView: "dayGridMonth",
-        events: this.calendarEvents,
-        headerToolbar: {
-          left: "dayGridMonth,timeGridWeek,timeGridDay",
-          center: "title",
-          right: "prevYear,prev,today,next,nextYear",
-        },
+        // header: {
+        //   left: "prev",
+        //   center: "title",
+        //   right: "next",
+        // },
+        // titleFormat: {
+        //   month: "MM YYYY",
+        //   week: "MM YYYY",
+        //   day: "MM YYYY",
+        // },
         height: "100vh",
+        dateClick: (info) => {
+          let calendarApi = this.$refs.fullCalendar.getApi();
+          // calendarApi.next()
+          calendarApi.gotoDate(info.dateStr);
+          calendarApi.changeView("timeGridDay");
+        },
         eventClick: function(info) {
           info.jsEvent.preventDefault(); // don't let the browser navigate
 
@@ -88,8 +125,14 @@ html,
 body {
   margin: 0;
   padding: 0;
-  /* font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
-  font-size: 14px; */
+  font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
+  font-size: 14px;
+}
+
+@media only screen and (max-device-width: 480px) {
+  body {
+    font-size: 10px;
+  }
 }
 /* 
 #calendar {
